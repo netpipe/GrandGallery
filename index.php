29881da309c0c03afbf8f22b425d1575173b0fe6
tecan <netpipe@linux-a1we.lan>
2019-02-21 00:28:53 -0700

    test

--- index.php
+++ index.php
@@ -1,26 +1,26 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xml:lang="en" lang="en" xmlns="http://www.w3.org/1999/xhtml">
 <head>
+    <meta name="Keywords" content="keywords" /><meta name="Description" content="so much fun you'll forget to..." />
 
-<meta name="Keywords" content="keywords" /><meta name="Description" content="so much fun you'll forget to..." />
-<!--ZOOMRESTART-->
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<!--         <meta http-equiv="refresh" content="50"> -->
-<style type="text/css" media="screen, projection">
-@import "style.css";
-</style>
-<title>Welcome To TFS.</title></head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta http-equiv="refresh" content="50"> <!--should be set higher for less traffic use DEVELOPMENT MODE-->
 
+
+    <style type="text/css" media="screen, projection">
+        @import "style.css";
+    </style>
+    <title>GrandGallery</title>
+</head>
+<!--body {
+  background: lightblue url("img_tree.gif") no-repeat fixed center;
+} -->
 <body style="background:#414141">
-
  <div id="wrapper">
-
   <div id="body">
-
-	 <div align="center" style="border:2px black solid;background: url('31.png');height:100%;width:100%;background:white;">
-	<img src="31.png" width="500px" />
-	</div>
-
+    <div align="center" style="border:2px black solid;background: url('www.test.com');height:100%;width:100%;background:white;">
+        <img src="logo/logo3.png" width="1000px" />
+    </div><!--logo-->
 	<div>
 	 <ul id="TJK_dropDownMenu">
 		<li id="AB" style="background:#616C7A ; width:12% "><a href="default.asp">Menu</a>
@@ -35,26 +35,30 @@
         <li id="3m" style="background:#3A455B ; width:12% "><a href="forums.php"> </a>	</li>
         <li id="3m" style="background:#2D3851;width:40%"> <a> <? print(date("D M d Y | G:i:s"));?></a> </li>
 		 </ul>
-	</div>
+	 </div> <!--menu-->
  <?php 
 //--background:#4A5568
 ?>
 
-<form action="upload.php" method="post" enctype="multipart/form-data">
-    Select image to upload:
-    <input type="file" name="fileToUpload" id="fileToUpload">
-    <input type="submit" value="Upload Image" name="submit">
-</form>
 
-    <?php
+
+    <form action="upload.php" method="post" enctype="multipart/form-data">
+        Select image to upload:
+        <input type="file" name="fileToUpload" id="fileToUpload">
+        <input type="submit" value="Upload Image" name="submit">
+    </form>
+    
+
+    
+<?php
             
 //         include("upload.php");
         
         
 function rendervideo($file){
- echo '<div style="margin-left:30px;">';
-    echo '<div style="background-color:#c1c1c1;height:200px;float:left;border:2px black solid;width:330px;">';
-        echo '<div style="margin-left:  background-color:black;margin:2px">';
+ //echo '<div style="background-color:red;margin-left:30px;">';
+    echo '<div id="videobox" style="margin:20px;background-color:#c1c1c1;height:200px;float:left;border:2px black solid;width:330px;" >';
+      //  echo '<div style="margin-left:  background-color:black;margin:2px">';
         
         echo '<div style="width:10%; text-align:center; float:left;background-color:black;height:20px;">';
         echo '</div>';
@@ -67,58 +71,56 @@ function rendervideo($file){
         echo '</div>';
         
        // echo '<div style="clear: both;vertical-align: middle;width:auto;height:auto" >';
-            echo '<div style="margin-left:13px;width:auto;height:auto" >';
-           // echo '<video controls  style="clear:both;" poster="title_anouncement.jpg" width="250">';
-                echo '<video controls style="" preload="none" loop="1" poster="title_anouncement.jpg" width="300">';
-                echo '<source src="'.$file.'"' .'type="video/webm";codecs="vp8, vorbis" />'; 
-            //  echo "enlarge video after play to see better"
-                echo '</video>';
-            echo '</div>';
-            
-       echo '</div>';
-   echo '</div>';
-echo '</div>';
-        /*
-        <h3>This is a heading</h3>
-        <p>This is a paragraph.</p>
-        <div style="background-color:green">
-        <p>This is a paragraph.</p>
-                //   echo rand(1, 10)."\n";
-        //  echo rand(1, 10)."<br>";
-        */
-        }  
+                echo '<div style="margin:20px;width:auto;height:auto" >';
+                 // echo '<video controls  style="clear:both;" poster="title_anouncement.jpg" width="250">';
+                    echo '<video controls style="" preload="none" loop="1" poster="title_anouncement.jpg" width="300">';
+                        //echo '<source src="'.$file.'"' .'type="video/webm";codecs="vp8, vorbis" />'; 
+                    echo '</video>';
+                echo '</div>';
+        
+        echo '</div>';
+  // echo '</div>';
+//echo '</div>';
 
         
-        echo '<div style="background-color:green; margin:20px" >';
-        rendervideo("test.webm");
-        rendervideo("test.webm"); 
-        rendervideo("test.webm"); 
-        rendervideo("test.webm");
-        rendervideo("test.webm"); 
-        rendervideo("test.webm"); 
-                rendervideo("test.webm");
-        rendervideo("test.webm"); 
-        rendervideo("test.webm"); 
-                rendervideo("test.webm");
-        rendervideo("test.webm"); 
-//         rendervideo("test.webm"); 
-//         rendervideo("test.webm");  
-//        rendervideo("test.webm");  
-//        rendervideo("test.webm");  
-        echo '</div>';        
+//         <h3>This is a heading</h3>
+//         <p>This is a paragraph.</p>
+//         <div style="background-color:green">
+//         <p>This is a paragraph.</p>
+//         //  echo rand(1, 10)."<br>";
+        
+        }  
+
+
+        echo '<div id="vbody" style="background-color:green; margin:20px;border:2px black solid;width:auto;height:auto" >';
+        
+        $files = scandir('uploads/');
+            foreach($files as $file) {
+            //do your work here
+            rendervideo($file);
+        }
+
+       // for ($x = 0; $x <= 1; $x++) {
+       // rendervideo("test.webm");
+       // } 
+
+      //  echo '</div>';  
+        
+        
       ?>
       
 
-	<div id="body" style="clear:both;border:1px black solid;background:white">
+	<div id="footer2" style="clear:both;border:1px black solid;background:white">
 		<h1>VideoHost</h1><br>
 		<img src="const.gif" /><br><br>
 		<abbr title="U">WIKI - comming soon</abbr>
 		<h2>under Construction</h2>
 	</div>
 	
-<br/>
+    <br>
 
   </div>
+  
  </div>
 </body>
 </html>
